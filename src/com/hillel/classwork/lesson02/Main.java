8eab1f4025d3a7a427eb7d806c41b0c35646ef01
dannnik4 <daniilperstnev2001@gmail.com>
2021-12-01 20:31:47 +0200

    Initial commit

--- src/com/hillel/classwork/lesson02/Main.java
+++ src/com/hillel/classwork/lesson02/Main.java
@@ -1,28 +1,30 @@
 package com.hillel.classwork.lesson02;
 
+/**
+ * Main class
+ */
 public class Main {
 
+  /*
+  psvm - create main method
+  sout - print to console
+  Ctrl + Alt + L - format the code
+  */
 
-
-
-
-    /*
-    sout - print to console
-    Ctrl + Alt + L - format code
-    */
 
     /**
      * Main method
      * @param args
      */
-
     // psvm
     public static void main(String[] args) {
 
         // sout
-        System.out.print("Hello In Idea");
+        System.out.print("Hello in Idea!");
         System.out.print(" the same line");
         System.out.println();
         System.out.println("row 2");
+
     }
+
 }
