550a52e39498f513b417e6dea505282e0dd8cd05
dannnik4 <daniilperstnev2001@gmail.com>
2021-12-03 12:03:25 +0200

    Initial commit

--- src/com/hillel/homework/lesson02/Task2.java
+++ src/com/hillel/homework/lesson02/Task2.java
@@ -2,6 +2,21 @@ package com.hillel.homework.lesson02;
 
 public class Task2 {
 
+    public static void main(String[] args) {
 
+        int a = 10;
+        int b = 20;
+
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b + "\n");
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
+    }
 
 }
