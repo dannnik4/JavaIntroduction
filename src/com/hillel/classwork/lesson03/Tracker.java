53067f591f8ff29cd14f9a61f6581743d2eafeb8
dannnik4 <daniilperstnev2001@gmail.com>
2021-12-06 11:34:48 +0200

    Initial commit

---
+++ src/com/hillel/classwork/lesson03/Tracker.java
@@ -0,0 +1,16 @@
+package com.hillel.classwork.lesson03;
+
+public class Tracker {
+
+    public static void main(String[] args) {
+
+        int steps = 10;
+        int time = 12;
+
+        float result = (float) steps / time;
+
+        System.out.println(result);
+
+    }
+
+}
