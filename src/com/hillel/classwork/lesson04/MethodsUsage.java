efb47f8ffb90dafefd36379bc3651866c1fa85f0
dannnik4 <daniilperstnev2001@gmail.com>
2021-12-10 17:44:29 +0200

    Add Lesson04 Classwork

---
+++ src/com/hillel/classwork/lesson04/MethodsUsage.java
@@ -0,0 +1,12 @@
+package com.hillel.classwork.lesson04;
+
+public class MethodsUsage {
+
+    public static void main(String[] args) {
+
+        Methods.printMessage();
+        System.out.println(Methods.generateRandom());
+
+    }
+
+}
