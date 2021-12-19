bc1837bc2a9675522091d6107c5e567c9cda6bc7
dannnik4 <daniilperstnev2001@gmail.com>
2021-12-19 18:00:34 +0200

    Add Lesson05 Homework

---
+++ src/com/hillel/classwork/lesson06/StringOverloading.java
@@ -0,0 +1,30 @@
+package com.hillel.classwork.lesson06;
+
+public class StringOverloading {
+
+    public static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        String test = READER.readLine();
+        String test2 = READER.readLine();
+
+        printConcatenation(test, test2);
+        printConcatenation(test, "any text");
+        printConcatenation("something", test);
+        printConcatenation("1", "2");
+        printConcatenation(READER.readLine(), READER.readLine());
+
+        printConcatenation("test", 1);
+
+    }
+
+    public static void printConcatenation(String s1, String s2) {
+        System.out.println(s1 + s2);
+    }
+
+    public static void printConcatenation(String str, int number) {
+        System.out.println(str + number);
+    }
+
+}
