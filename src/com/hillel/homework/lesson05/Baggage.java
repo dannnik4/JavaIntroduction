9ca66ce4e366da9af1b27c83df4defbf1651abec
Dannnik <daniilperstnev2001@gmail.com>
2021-12-24 18:41:16 +0200

    Add Lesson05 Homework

--- src/com/hillel/homework/lesson05/Baggage.java
+++ src/com/hillel/homework/lesson05/Baggage.java
@@ -17,7 +17,11 @@ public class Baggage {
         System.out.println("Введите длину глубины: ");
         int glubina = Integer.parseInt(reader.readLine());
 
-        if (shirina <= 40 && (dlina + glubina) <= 75) {
+        if (shirina <= 40 && dlina <= 55 && glubina <= 20) {
+            System.out.println("Багаж подходит для ручной клади");
+        } else if (shirina <= 40 && dlina <= 54 && glubina <= 21) {
+            System.out.println("Багаж подходит для ручной клади");
+        } else if (shirina <= 40 && dlina <= 53 && glubina <= 22) {
             System.out.println("Багаж подходит для ручной клади");
         } else {
             System.out.println("Багаж не подходит для ручной клади");
