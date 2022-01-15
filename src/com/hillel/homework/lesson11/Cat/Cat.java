59ef9345316b5a49b9a414c89b5ce65a132ba2c4
dannnik4 <daniilperstnev2001@gmail.com>
2022-01-15 21:32:31 +0200

    Add Lesson11 Homework

---
+++ src/com/hillel/homework/lesson11/Cat/Cat.java
@@ -0,0 +1,46 @@
+package com.hillel.homework.lesson11.Cat;
+
+public class Cat {
+
+    String name;
+    int age;
+    int weight;
+    String breed;
+
+    public Cat() {
+        System.out.println("Cat has been created!");
+    }
+
+    public Cat(String name, int age, int weight, String breed) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.breed = breed;
+    }
+
+    public void run() {
+        System.out.println("cat runs");
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof Cat) {
+            Cat cat = (Cat) obj;
+            return this.name.equals(cat.name) && this.age == cat.age;
+        }
+        if (obj instanceof Cat) {
+            Cat cat = (Cat) obj;
+            return this.name.equals(cat.name) && this.weight == cat.weight;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat {" +
+                "name='" + this.name + '\'' +
+                ", size='" + this.age +
+                ", age=" + this.weight +
+                ", color='" + this.breed + '\'' +
+                '}';
+    }
+}
---
+++ src/com/hillel/homework/lesson11/Cat/CatMain.java
@@ -0,0 +1,42 @@
+package com.hillel.homework.lesson11.Cat;
+
+public class CatMain {
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        cat.run();
+
+        cat.name = "Mastiff";
+        cat.age = 7;
+        cat.weight = 10;
+        cat.breed = "Black";
+
+        System.out.println(cat.name);
+        System.out.println(cat.age);
+        System.out.println(cat.weight);
+        System.out.println(cat.breed);
+
+        Cat cat2 = new Cat();
+
+        cat2.name = "Maltese";
+        cat2.age = 8;
+        cat2.weight = 6;
+        cat2.breed = "White";
+
+        System.out.println(cat2.name);
+        System.out.println(cat2.age);
+        System.out.println(cat2.weight);
+        System.out.println(cat2.breed);
+
+        cat2.run();
+
+        System.out.println(cat);
+        System.out.println(cat2);
+
+        System.out.println(cat == cat2);
+        System.out.println(cat.equals(cat2));
+
+    }
+
+}
