8715032ed033e3968904575d12e0e66ec18fe9d8
dannnik4 <daniilperstnev2001@gmail.com>
2022-01-15 18:41:44 +0200

    Add Lesson08 Homework

--- src/com/hillel/homework/lesson08/ArrayCopy.java
+++ src/com/hillel/homework/lesson08/ArrayCopy.java
@@ -1,15 +1,22 @@
 package com.hillel.homework.lesson08;
 
-import static com.hillel.classwork.lesson09.ArraySort.fill;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class ArrayCopy {
 
-    public static void main(String[] args) {
+    static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
 
         int numbers[] = new int[20];
-        fill(numbers);
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(READER.readLine());
+        }
+
         System.out.println(Arrays.toString(numbers));
         int[] num1 = Arrays.copyOfRange(numbers, 0, 10);
         int[] num2 = Arrays.copyOfRange(numbers, 10, 20);
