0f6ade554e2773509918078904d0b5af18a86578
dannnik4 <daniilperstnev2001@gmail.com>
2022-02-01 18:51:30 +0200

    Add Lesson13 Classwork

---
+++ src/com/hillel/classwork/lesson13/Dog.java
@@ -0,0 +1,19 @@
+package com.hillel.classwork.lesson13;
+
+public class Dog extends Animal {
+
+    public Dog() {
+        super("black");
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("gav");
+        action();
+    }
+
+    public void action() {
+        System.out.println("виляет хвостиком");
+    }
+
+}
