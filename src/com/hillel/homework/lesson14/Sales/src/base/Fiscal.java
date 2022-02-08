9b593fd16e257d10f18dc6a911f2416abbc1285e
dannnik4 <95372410+dannnik4@users.noreply.github.com>
2022-02-08 18:34:45 +0200

    Delete src/com/hillel/homework/lesson14/Sales directory

--- src/com/hillel/homework/lesson14/Sales/src/base/Fiscal.java
+++
@@ -1,7 +0,0 @@
-package com.hillel.homework.lesson14.Sales.src.base;
-
-public interface Fiscal {
-
-    double taxFromIncome(double income);
-
-}
--- src/com/hillel/homework/lesson14/Sales/src/base/Income.java
+++
@@ -1,8 +0,0 @@
-package com.hillel.homework.lesson14.Sales.src.base;
-
-public interface Income {
-
-    double calculateIncome(int quantity, double price);
-    double calculateIncome(double income, double taxFromIncome);
-
-}
